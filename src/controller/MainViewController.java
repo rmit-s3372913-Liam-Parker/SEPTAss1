f4fdda876ed28e721e638c808b1a7b12d6816e06
Liam Parker - s3372913 <s3372913@student.rmit.edu.au>
2016-03-31 14:14:25 +1100

    Main view controller implemented

--- src/controller/MainViewController.java
+++ src/controller/MainViewController.java
@@ -1,6 +1,33 @@
 package controller;
 
-public class MainViewController 
-{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+
+public class MainViewController implements ActionListener 
+{
+	JFrame stations, favorites;
+	
+	public MainViewController(JFrame stations, JFrame favorites)
+	{
+		this.stations = stations;
+		this.favorites = favorites;
+	}
+	
+	// Lazy with a switch, should probably extract each
+	// button to its own listener at some point.
+	@Override
+	public void actionPerformed(ActionEvent arg0)
+	{
+		switch(arg0.getActionCommand())
+		{
+		case "Weather Stations":
+			stations.setVisible(true);
+			break;
+		case "Favourites":
+			favorites.setVisible(true);
+			break;
+		}
+	}
 }

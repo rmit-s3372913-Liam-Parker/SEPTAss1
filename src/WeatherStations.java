9a0b8cfd680ece0a866b4d5162aeb45bf0124361
Sarah Pignatelli <s3546818@student.rmit.edu.au>
2016-03-31 18:26:17 +1100

    tweaking

--- src/WeatherStations.java
+++ src/WeatherStations.java
@@ -3,29 +3,29 @@ import javax.swing.*;
 
 public class WeatherStations extends JFrame
 {
-	public void Stations()
+	
+	JPanel panel1 = new JPanel();
+	
+	public WeatherStations()
 	{
-		JFrame frame = new JFrame();
+		panel1.setLayout(new GridLayout(6,2));
+		setBounds(5,5,100,100);
+		panel1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		JScrollPane scrollPane = new JScrollPane();
 		
-		JPanel panel1 = new JPanel();
 		
-		
-		panel1.setLayout(new GridLayout(5,2));
-		
-		for(int i= 0; i<=10; i++)
+		for(int i= 0; i<=9; i++)
 		{
 			panel1.add(new JLabel("" + i)); //numbers each cell
 		}
+
+		this.add(scrollPane);
+		this.add(panel1);
+		this.setTitle("Weather Stations | Weather App");
+		this.setSize(400,400); //needs to be changed
+		this.setLocationRelativeTo(null); //centre the frame - needs to be changed
+		this.setMinimumSize(new Dimension(400,400));
 		
-		
-		//JPanel panel2 = new JPanel(new);
-		
-		//this.add(panel);
-		frame.setTitle("Weather Stations | Weather App");
-		frame.setSize(400,400); //needs to be changed
-		frame.setLocationRelativeTo(null); //centre the frame - needs to be changed
-		frame.setVisible(true);
-		frame.setMinimumSize(new Dimension(400,400));
 	}
 	
 	

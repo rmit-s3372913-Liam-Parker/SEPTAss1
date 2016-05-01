be3ed434fe2abf05dcbd7e9fb16b9d6bfa1055b3
Liam Parker - s3372913 <s3372913@student.rmit.edu.au>
2016-05-02 02:35:30 +1000

    Weather Stations View Layout Changes

--- src/view/WeatherStationsView.java
+++ src/view/WeatherStationsView.java
@@ -1,19 +1,28 @@
 package view;
 
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 
 import org.json.JSONObject;
 
 import interfaces.IJsonSerializable;
 import interfaces.IWeatherSystemCallback;
 import interfaces.WeatherSystem;
+import model.State;
 import model.WeatherStation;
 
 
@@ -25,6 +34,11 @@ public class WeatherStationsView extends JPanel implements IWeatherSystemCallbac
 	
 	JScrollPane scrollPane;
 	JPanel panel = new JPanel();
+	
+	JPanel filterPanel = new JPanel();
+	JComboBox<State> stateFilter = new JComboBox<State>();
+	JTextField filterTextField = new JTextField();
+	
 	List<StationEntryView> entryList = new ArrayList<StationEntryView>();
 	
 	JButton buttonFavourite = new JButton("Favourite");
@@ -46,6 +60,16 @@ public class WeatherStationsView extends JPanel implements IWeatherSystemCallbac
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		
+		TitledBorder border = BorderFactory.createTitledBorder("Weather Station Filter");
+		border.setTitleJustification(TitledBorder.CENTER);
+		filterPanel.setBorder(border);
+		filterPanel.add(stateFilter);
+		filterPanel.add(filterTextField);
+		this.add(filterPanel);
+		
+		panel.setBorder(new EmptyBorder(15,15,15,15));
+		//panel.add(Box.createRigidArea(new Dimension(7.5)));
+		
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		this.add(scrollPane);
@@ -55,6 +79,8 @@ public class WeatherStationsView extends JPanel implements IWeatherSystemCallbac
 	public void Refresh() 
 	{
 		List<WeatherStation> favorites = system.getWeatherStations();
+		
+		entryList.clear();
 		
 		for(WeatherStation station : favorites)
 		{

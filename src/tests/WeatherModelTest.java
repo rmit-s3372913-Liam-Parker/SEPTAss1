ca81e8057d3f6b0467ce6a63aa20983dd8004a30
rmit-s3372913-Liam-Parker <s3372913@student.rmit.edu.au>
2016-05-17 18:36:07 +1000

    Adding extra test for travis tests

---
+++ src/tests/WeatherModelTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WeatherModelTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

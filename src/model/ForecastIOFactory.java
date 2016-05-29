54943628cec7baa981f78a8fabc815a977122c00
rmit-s3494334-michael-swiatkiwsky <s3494334@student.rmit.edu.au>
2016-05-30 02:55:04 +1000

    removed  old code that was commented out

--- src/model/ForecastIOFactory.java
+++ src/model/ForecastIOFactory.java
@@ -56,43 +56,7 @@ public class ForecastIOFactory implements ForecastFactory {
         // https://developer.forecast.io/docs/v2
 		while (entriesItr.hasNext()) {
 			JSONObject entry = (JSONObject) entriesItr.next();
-
-			
-			
-			
-			
-/*			// For each recording, grab the relevant info
-			// Date parsing
-			long unixTime = entry.getLong("time"); // daily represents seconds to midnight in API
-
-			Date date = new Date(unixTime*1000L);
-
-			// Temperatures and humidity
-			float temp = (float) entry.optDouble("temperature");
-			float appTemp = (float) entry.optDouble("apparentTemperatureMax");
-			float dewPoint = (float) entry.optDouble("dewPoint");
-			int relHum = entry.optInt("humidity");
-			float deltaT = 0.0f;// = (float) entry.optDouble("delta_t"); TODO: figure out equiv in API
-
-			// Wind & gust //TODO figure out enum conversion.
-			CompassDirection windDir = CompassDirection.CALM; //entry.optEnum(CompassDirection.class, "wind_dir");
-			int windSpeedKmh = (int)(entry.optInt("windSpeed") * MPH_2_KMH); // API returns MPH
-			int gustSpeedKmh = 0;// = entry.optInt("gust_kmh");
-			int windSpeedKts = (int)(windSpeedKmh * KMH_2_KTS);
-			int gustSpeedKts = 0;// entry.optInt("gust_kt");
-			// Pressure
-			float millibarPres = (float) entry.optDouble("press_qnh");
-			float pressQNH = millibarPres; // TODO conversions
-			float pressMSL = millibarPres;
-			// Precipitation
-			float rainSinceNineAM = (float) entry.optDouble("precipAccumulation");
-			// Create a snapshot entry using all the scraped data
-			WeatherDataPoint snapshotEntry = new WeatherDataPoint(date, temp, appTemp, dewPoint, relHum, deltaT,
-					windDir, windSpeedKmh, gustSpeedKmh, windSpeedKts, gustSpeedKts, pressQNH, pressMSL,
-					rainSinceNineAM);*/
-			
-
-			
+		
 			//Summary
 			long unixTime = entry.optLong("time");
 			Date date = new Date(unixTime*1000L);

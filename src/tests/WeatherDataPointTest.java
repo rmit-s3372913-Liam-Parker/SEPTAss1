229d627334b456ae9241f889e834ed0c87be4ff2
rmit-s3372913-Liam-Parker <s3372913@student.rmit.edu.au>
2016-05-30 05:23:48 +1000

    Tests

---
+++ src/tests/WeatherDataPointTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import model.CompassDirection;
+import model.WeatherDataPoint;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+public class WeatherDataPointTest
+{
+    //Date date, float temp, float appTemp, float dewPoint, int relHum, float deltaT,
+    //CompassDirection windDir, int windSpeedKm, int gustSpeedKm, int windSpeedKts, int gustSpeedKts,
+    //float pressQNH, float pressMSL, float rainSinceNineAM
+
+    WeatherDataPoint dataPoint;
+    Date date = new Date();
+    @Before
+    public void setUp() throws Exception {
+        dataPoint = new WeatherDataPoint(date, 15f,15f, 1f, 2, 2f, CompassDirection.CALM, 2, 2, 4, 4, 1000f, 1000f, 22);
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals(dataPoint.getDate(), date);
+        assertEquals(dataPoint.getTemp(), 15f, 0.1f);
+        assertEquals(dataPoint.getAppTemp(), 15f, 0.1f);
+        assertEquals(dataPoint.getDewPoint(), 1f, 0.1f);
+        assertEquals(dataPoint.getRelHum(), 2);
+        assertEquals(dataPoint.getDeltaT(), 2f, 0.1f);
+        assertEquals(dataPoint.getWindDir(), CompassDirection.CALM);
+        assertEquals(dataPoint.getWindSpeedKmh(), 2);
+        assertEquals(dataPoint.getGustSpeedKmh(), 2);
+        assertEquals(dataPoint.getWindSpeedKts(), 4);
+        assertEquals(dataPoint.getGustSpeedKts(), 4);
+        assertEquals(dataPoint.getPressQNH(), 1000f, 0.1f);
+        assertEquals(dataPoint.getPressMSL(), 1000f, 0.1f);
+        assertEquals(dataPoint.getRainSinceNineAM(), 22, 0.1);
+    }
+}

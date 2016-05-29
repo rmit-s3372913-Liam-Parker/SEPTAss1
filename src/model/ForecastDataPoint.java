b34fe13e6e7dbd20e491a01c2e8f857b483da4fc
rmit-s3372913-Liam-Parker <s3372913@student.rmit.edu.au>
2016-05-30 04:19:32 +1000

    push

--- src/model/ForecastDataPoint.java
+++ src/model/ForecastDataPoint.java
@@ -4,70 +4,68 @@ import java.util.Date;
 
 public class ForecastDataPoint
 {
-		//Summary
-		private Date date;
-		private String summary;
+	//Summary
+	private Date date;
+	private String summary;
+	// Temperatures and humidity
+	private float temp;
+	//private float appTemp; //removing because only forecast.io has this, weathermap does not
+	private int relHum;
+	// Wind & gust
+	private int windDir;
+	private float windSpeedKmh;
+	// Pressure
+	private float pressure;
+	// Precipitation
+	public ForecastDataPoint(Date date, String summary, float temp, int relHum, int windDir, float windSpeedKmh, float pressure)
+	{
+		this.date = date;
+		this.summary = summary;
+		this.temp = temp;
+		this.relHum = relHum;
+		this.windDir = windDir;
+		this.windSpeedKmh = windSpeedKmh;
+		this.pressure = pressure;
+	}
+	public Date getDate()
+	{
+		return date;
+	}
 
-		// Temperatures and humidity
-		private float temp;
-		//private float appTemp; //removing because only forecast.io has this, weathermap does not
-		private int relHum;
+	public String getSummary()
+	{
+		return summary;
+	}
 
-		// Wind & gust
-		private int windDir;
-		private float windSpeedKmh;
+	public float temp()
+	{
+		return temp;
+	}
 
-		// Pressure
-		private float pressure;
+	public int relHum()
+	{
+		return relHum;
+	}
 
-		// Precipitation
+	public int windDir()
+	{
+		return windDir;
+	}
 
-		public ForecastDataPoint(Date date, String summary, float temp, int relHum, int windDir, 
-				float windSpeedKmh, float pressure)
-		{
-				this.date = date;
-				this.summary = summary;
-				this.temp = temp;
-				this.relHum = relHum;
-				this.windDir = windDir;
-				this.windSpeedKmh = windSpeedKmh;
-				this.pressure = pressure;
-		}
+	public float windSpeedKmh()
+	{
+		return windSpeedKmh;
+	}
 
-		public Date getDate()
-		{
-			return date;
-		}
-		
-		public String getSummary()
-		{
-			return summary;
-		}
-		
-		public float temp()
-		{
-			return temp;
-		}
-		
-		public int relHum()
-		{
-			return relHum;
-		}
-		
-		public int windDir()
-		{
-			return windDir;
-		}
-		
-		public float windSpeedKmh()
-		{
-			return windSpeedKmh;
-		}
-		
-		public float pressure()
+	public float pressure()
 		{
 			return pressure;
 		}
 
-	
+	public String toString()
+	{
+		return String.format("Date: %s | Temp: %.2f | Rel Hum: %d | Wind Dir: %d | Wind Spd Kmh: %.2f"
+						+ "| Pressure: %2.f",
+				date.toString(), temp, relHum, windDir, windSpeedKmh, pressure);
+	}
 }
--- src/model/ForecastFactory.java
+++ src/model/ForecastFactory.java
@@ -28,11 +28,4 @@ public abstract class ForecastFactory {
 
 	double MPH_2_KMH = 1.609344; //TODO: Move to interface perhaps??
 	double KMH_2_KTS = 0.539956; // conversion macros
-	
-	public String toString()
-	{
-		return String.format("Date: %s | Temp: %.2f | Rel Hum: %d | Wind Dir: %d | Wind Spd Kmh: %.2f"
-				+ "| Pressure: %2.f",
-				date.toString(), temp, relHum, windDir, windSpeedKmh, pressure);
-	}
 }

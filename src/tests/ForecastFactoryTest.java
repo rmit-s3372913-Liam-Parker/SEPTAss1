58442fe280716ac35ef2e534d46a9d23c0f8162e
rmit-s3372913-Liam-Parker <s3372913@student.rmit.edu.au>
2016-05-30 04:46:32 +1000

    Forecast Factory test

---
+++ src/tests/ForecastFactoryTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import interfaces.WeatherSystem;
+import model.WeatherModelImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ForecastFactoryTest
+{
+    WeatherSystem model;
+
+    @Before
+    public void setUp() throws Exception {
+        model = new WeatherModelImpl("test_stations.json");
+    }
+
+    @Test
+    public void addFavoriteStationTestA() {
+        // Test for inserting null name
+        assertFalse(model.addFavoriteStation(null));
+    }
+
+    @Test
+    public void addFavoriteStationTestB() {
+        assertTrue(model.addFavoriteStation("Test Station 1"));
+    }
+
+    @Test
+    public void addFavoriteStationTestC() {
+        assertFalse(model.addFavoriteStation("Invalid Station - faasddahwerbdf"));
+    }
+}
